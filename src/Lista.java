3533ce62ce855a90d112760d42a8ee2c055be42b
nicktoon21 <nicolas98aguiar@gmail.com>
2019-08-19 17:51:05 -0300

    Lista com inclusão/retirada no inicio e no fim.

--- src/Lista.java
+++ src/Lista.java
@@ -5,7 +5,7 @@ public class Lista {
 
     Scanner read = new Scanner(System.in);
 
-    private int quantidadeDeElementos;
+    private int quantidadeDeElementos = 0;
     private No inicio;
     private No fim;
 
@@ -14,32 +14,32 @@ public class Lista {
     }
 
     //Inserir no inicio
-    public void InserirInicio(int valor){
-        No proximo = null;
+    public void inserirInicio(int valor){
+        No ponteiro = null;
         try{
-            proximo = new No(valor);
+            ponteiro = new No(valor);
         }catch(OutOfMemoryError error){
             System.out.printf("\nErro: Estouro de memoria! (%s)", error.getMessage());
         }
-        if(getQuantidadeDeElementos() < 1){
-            inicio = proximo;
-            fim = proximo;
-            proximo.setAnterior(proximo);
-            proximo.setProximo(proximo);
+        if(quantidadeDeElementos < 1){
+            inicio = ponteiro;
+            fim = ponteiro;
+            ponteiro.setAnterior(ponteiro);
+            ponteiro.setProximo(ponteiro);
         }else{
-            proximo.setProximo(inicio);
-            proximo.setAnterior(fim);
-            fim.setProximo(proximo);
-            inicio.setAnterior(proximo);
-            inicio = proximo;
+            ponteiro.setProximo(inicio);
+            ponteiro.setAnterior(fim);
+            fim.setProximo(ponteiro);
+            inicio.setAnterior(ponteiro);
+            inicio = ponteiro;
         }
         quantidadeDeElementos++;
     }
 
     //Retirar do inicio
-    public int RetirarInicio(){
+    public int retirarInicio(){
         No ponteiroAux = null;
-        if(getQuantidadeDeElementos() <= 0){
+        if(quantidadeDeElementos <= 0){
             System.out.printf("\nQuantidade de elementos igual a 0! (%s)");
             return 0;
         }else if (quantidadeDeElementos == 1){
@@ -53,9 +53,53 @@ public class Lista {
             inicio.setAnterior(fim);
             ponteiroAux.setAnterior(null);
             ponteiroAux.setProximo(null);
-            quantidadeDeElementos--;
         }
+        quantidadeDeElementos--;
         return ponteiroAux.getValor();
     }
 
+    //Inserir no fim
+    public void inserirFim(int valor){
+        No ponteiro = null;
+        try{
+            ponteiro = new No(valor);
+        }catch(OutOfMemoryError error){
+            System.out.printf("\nErro: Estouro de memoria! (%s)", error.getMessage());
+        }
+        if(quantidadeDeElementos < 1){
+            inicio = ponteiro;
+            fim = ponteiro;
+            ponteiro.setAnterior(ponteiro);
+            ponteiro.setProximo(ponteiro);
+        }else{
+            fim.setProximo(ponteiro);
+            ponteiro.setAnterior(fim);
+            fim = ponteiro;
+            fim.setProximo(inicio);
+            inicio.setAnterior(fim);
+        }
+        quantidadeDeElementos++;
+    }
+
+    //Retirada Fim
+    public int retirarFim() {
+        No ponteiroAux = null;
+        if (quantidadeDeElementos <= 0) {
+            System.out.printf("\nQuantidade de elementos igual a 0! (%s)");
+            return 0;
+        } else if (quantidadeDeElementos == 1) {
+            ponteiroAux = inicio;
+            inicio.setProximo(null);
+            inicio.setAnterior(null);
+        } else {
+            ponteiroAux = fim;
+            fim = fim.getAnterior();
+            ponteiroAux.setAnterior(null);
+            ponteiroAux.setProximo(null);
+            fim.setProximo(inicio);
+            inicio.setAnterior(fim);
+        }
+        quantidadeDeElementos--;
+        return ponteiroAux.getValor();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main {
     public static void main(String[] args){
         Lista lista = new Lista();
 
-        lista.InserirInicio(10);
-        System.out.println("Valor:"+lista.RetirarInicio());
+        lista.inserirFim(9);
+        lista.inserirFim(5);
+        lista.inserirFim(4);
+        lista.inserirFim(2);
+
+        System.out.println("Valor:"+lista.retirarFim());
+        System.out.println("Valor:"+lista.retirarFim());
+        System.out.println("Valor:"+lista.retirarFim());
     }
 }
