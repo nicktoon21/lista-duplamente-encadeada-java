1482ee1a1e4dd66b52d3ed4e10839774bc7ad33f
nicktoon21 <51923372+nicktoon21@users.noreply.github.com>
2019-12-11 09:42:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+//Implementação de Lista Duplamente Encadeada Circular.
+
 public class Main {
     public static void main(String[] args){
+        //Instânciar Lista
         Lista lista = new Lista();
 
+        //Inserção dados na pelo fim da lista
         lista.inserirFim(9);
         lista.inserirFim(5);
         lista.inserirFim(4);
         lista.inserirFim(2);
 
+        //Retirando dados pelo fim da lista e exibindo.
         System.out.println("Valor:"+lista.retirarFim());
         System.out.println("Valor:"+lista.retirarFim());
         System.out.println("Valor:"+lista.retirarFim());
